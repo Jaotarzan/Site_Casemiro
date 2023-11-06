8e1f3f153ca6c0299249b8c4ffebfccdee24efcd
Jaotarzan <joaovssouza59@gmail.com>
2023-11-06 20:12:18 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// Adicionando eventos de clique aos botões para redirecionar à página inicial
+document.getElementById('button1').onclick = function() {
+    window.location.href = "index.html";
+}
+
+document.getElementById('button2').onclick = function() {
+    window.location.href = "index.html";
+}
+
+document.getElementById('button3').onclick = function() {
+    window.location.href = "index.html";
+}
+
+document.getElementById('button4').onclick = function() {
+    window.location.href = "index.html";
+}
+
+document.getElementById('button5').onclick = function() {
+    window.location.href = "index.html";
+}
+
+document.getElementById('button6').onclick = function() {
+    window.location.href = "index.html";
+}
